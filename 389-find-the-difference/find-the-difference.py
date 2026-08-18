4e0ba87eb7a9465bd56b9274a9628f4398fa9343
Pratham singla <pratham83111@gmail.com>
2026-08-18 20:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.92%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        ans = 0
+
+        for ch in s + t:
+            ans ^= ord(ch)
+
+        return chr(ans)
+        
