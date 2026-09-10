a38474c54a4d93f91bf0422b4feaac42c7b01760
Pratham singla <pratham83111@gmail.com>
2026-09-10 12:28:05 +0530

    Time: 22 ms (50.10%) | Memory: 13.3 MB (39.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,5 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+class Solution(object):
+    def isAnagram(self, s, t):
+        self.s = s
+        self.t = t
         return sorted(s)==sorted(t)
